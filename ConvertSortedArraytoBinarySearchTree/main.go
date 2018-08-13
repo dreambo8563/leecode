cee07684b862d42e38df1bcb52b9ab50f0562f06
dreambo8563 <dreambo8563@outlook.com>
2018-08-13 22:39:00 +0800

    format

--- ConvertSortedArraytoBinarySearchTree/main.go
+++ ConvertSortedArraytoBinarySearchTree/main.go
@@ -17,10 +17,6 @@
 
 package main
 
-import (
-	"fmt"
-)
-
 //TreeNode -
 type TreeNode struct {
 	Val   int
@@ -30,37 +26,11 @@ type TreeNode struct {
 
 func main() {
 	input := []int{-10, -3, 0, 5, 9}
-	// tree := sortedArrayToBST(input)
-	tree := sortedArrayToBST(input)
-	printTree(tree)
+	sortedArrayToBST(input)
 }
-
-// func sortedArrayToBST(nums []int) *TreeNode {
-// 	return addNode(nums)
-// }
 
 func sortedArrayToBST(nums []int) *TreeNode {
 	length := len(nums)
-	fmt.Println(length)
-	// if length == 3 {
-	// 	return &TreeNode{
-	// 		Val: nums[1],
-	// 		Left: &TreeNode{
-	// 			Val: nums[0],
-	// 		},
-	// 		Right: &TreeNode{
-	// 			Val: nums[2],
-	// 		},
-	// 	}
-	// }
-	// if length == 2 {
-	// 	return &TreeNode{
-	// 		Val: nums[1],
-	// 		Left: &TreeNode{
-	// 			Val: nums[0],
-	// 		},
-	// 	}
-	// }
 	if length == 1 {
 		return &TreeNode{
 			Val: nums[0],
@@ -69,28 +39,11 @@ func sortedArrayToBST(nums []int) *TreeNode {
 	if length == 0 {
 		return nil
 	}
-	fmt.Printf("first part: %v \n", nums[0:(length-1)/2+1])
-	fmt.Printf("second part: %v \n", nums[(length-1)/2+1:])
 
-	// splitArr(nums[length/2:])
 	return &TreeNode{
 		Val:   nums[(length)/2],
 		Left:  sortedArrayToBST(nums[0 : (length)/2]),
 		Right: sortedArrayToBST(nums[(length)/2+1:]),
-	}
-}
-
-func printTree(tree *TreeNode) {
-	if tree != nil {
-		fmt.Printf("tree %v \n", tree)
-		if tree.Left != nil {
-			fmt.Println("left node")
-			printTree(tree.Left)
-		}
-		if tree.Right != nil {
-			fmt.Println("right node")
-			printTree(tree.Right)
-		}
 	}
 }
 
